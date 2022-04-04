60afe88e0eaaf51d99528e3eecb7d5f891a1b288
Johan.Alger <johan.alger@gmail.com>
2022-04-04 15:16:52 +0200

    First commit front

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,95 +1,94 @@
-
-import { ROUTES_PATH } from '../constants/routes.js'
-export let PREVIOUS_LOCATION = ''
+import { ROUTES_PATH } from "../constants/routes.js";
+export let PREVIOUS_LOCATION = "";
 
 // we use a class so as to test its methods in e2e tests
 export default class Login {
   constructor({ document, localStorage, onNavigate, PREVIOUS_LOCATION, store }) {
-    this.document = document
-    this.localStorage = localStorage
-    this.onNavigate = onNavigate
-    this.PREVIOUS_LOCATION = PREVIOUS_LOCATION
-    this.store = store
-    const formEmployee = this.document.querySelector(`form[data-testid="form-employee"]`)
-    formEmployee.addEventListener("submit", this.handleSubmitEmployee)
-    const formAdmin = this.document.querySelector(`form[data-testid="form-admin"]`)
-    formAdmin.addEventListener("submit", this.handleSubmitAdmin)
+    this.document = document;
+    this.localStorage = localStorage;
+    this.onNavigate = onNavigate;
+    this.PREVIOUS_LOCATION = PREVIOUS_LOCATION;
+    this.store = store;
+    const formEmployee = this.document.querySelector(`form[data-testid="form-employee"]`);
+    formEmployee.addEventListener("submit", this.handleSubmitEmployee);
+    const formAdmin = this.document.querySelector(`form[data-testid="form-admin"]`);
+    formAdmin.addEventListener("submit", this.handleSubmitAdmin);
   }
-  handleSubmitEmployee = e => {
-    e.preventDefault()
+  handleSubmitEmployee = (e) => {
+    e.preventDefault();
     const user = {
       type: "Employee",
       email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
-      status: "connected"
-    }
-    this.localStorage.setItem("user", JSON.stringify(user))
+      status: "connected",
+    };
+    this.localStorage.setItem("user", JSON.stringify(user));
     this.login(user)
-      .catch(
-        (err) => this.createUser(user)
-      )
+      .catch((err) => this.createUser(user))
       .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-        this.PREVIOUS_LOCATION = ROUTES_PATH['Bills']
-        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-        this.document.body.style.backgroundColor="#fff"
-      })
+        this.onNavigate(ROUTES_PATH["Bills"]);
+        this.PREVIOUS_LOCATION = ROUTES_PATH["Bills"];
+        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION;
+        this.document.body.style.backgroundColor = "#fff";
+      });
+  };
 
-  }
-
-  handleSubmitAdmin = e => {
-    e.preventDefault()
+  handleSubmitAdmin = (e) => {
+    e.preventDefault();
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
-      status: "connected"
-    }
-    this.localStorage.setItem("user", JSON.stringify(user))
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
+      status: "connected",
+    };
+    this.localStorage.setItem("user", JSON.stringify(user));
     this.login(user)
-      .catch(
-        (err) => this.createUser(user)
-      )
+      .catch((err) => this.createUser(user))
       .then(() => {
-        this.onNavigate(ROUTES_PATH['Dashboard'])
-        this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard']
-        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION
-        document.body.style.backgroundColor="#fff"
-      })
-  }
+        this.onNavigate(ROUTES_PATH["Dashboard"]);
+        this.PREVIOUS_LOCATION = ROUTES_PATH["Dashboard"];
+        PREVIOUS_LOCATION = this.PREVIOUS_LOCATION;
+        document.body.style.backgroundColor = "#fff";
+      });
+  };
 
   // not need to cover this function by tests
   login = (user) => {
     if (this.store) {
       return this.store
-      .login(JSON.stringify({
-        email: user.email,
-        password: user.password,
-      })).then(({jwt}) => {
-        localStorage.setItem('jwt', jwt)
-      })
+        .login(
+          JSON.stringify({
+            email: user.email,
+            password: user.password,
+          })
+        )
+        .then(({ jwt }) => {
+          localStorage.setItem("jwt", jwt);
+        });
     } else {
-      return null
+      return null;
     }
-  }
+  };
 
   // not need to cover this function by tests
   createUser = (user) => {
     if (this.store) {
       return this.store
-      .users()
-      .create({data:JSON.stringify({
-        type: user.type,
-        name: user.email.split('@')[0],
-        email: user.email,
-        password: user.password,
-      })})
-      .then(() => {
-        console.log(`User with ${user.email} is created`)
-        return this.login(user)
-      })
+        .users()
+        .create({
+          data: JSON.stringify({
+            type: user.type,
+            name: user.email.split("@")[0],
+            email: user.email,
+            password: user.password,
+          }),
+        })
+        .then(() => {
+          console.log(`User with ${user.email} is created`);
+          return this.login(user);
+        });
     } else {
-      return null
+      return null;
     }
-  }
+  };
 }
