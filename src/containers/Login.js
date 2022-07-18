81ffd3e045217359f99c555796ac3ea4fb716f29
ultimecreation <naserbechata@gmail.com>
2022-07-18 15:34:15 +0200

    fixed form errors

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -41,10 +41,11 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
+   
     this.localStorage.setItem("user", JSON.stringify(user))
     this.login(user)
       .catch(
@@ -60,6 +61,7 @@ export default class Login {
 
   // not need to cover this function by tests
   login = (user) => {
+   
     if (this.store) {
       return this.store
       .login(JSON.stringify({
