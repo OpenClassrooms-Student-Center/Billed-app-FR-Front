006687e09bb588901a9af813030dfd2f0bd595cd
ultimecreation <naserbechata@gmail.com>
2022-07-23 19:58:18 +0200

    added expect icon on employee bill page

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -26,7 +26,7 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
+      expect(windowIcon.classList).toContain('active-icon')
     })
     test("Then bills should be ordered from earliest to latest",  () => {
       
