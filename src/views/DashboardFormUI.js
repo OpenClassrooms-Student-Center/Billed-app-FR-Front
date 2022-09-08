d6e708e894a6f3fd8f5aadb1967dbdb49706c05c
Khaera MB <khaera.belkadi@gmail.com>
2022-09-08 13:46:34 +0200

    Fix dashBoard handleClickEvent and  Newbills Test / newbills error .extent fix

--- src/views/DashboardFormUI.js
+++ src/views/DashboardFormUI.js
@@ -22,7 +22,7 @@ export const modal = () => (`
   `)
 
 export default (bill) => {
-
+console.log(bill)
   return (`
     <div class="container dashboard-form" data-testid="dashboard-form">
       <div class="row">
@@ -68,7 +68,7 @@ export default (bill) => {
         <div class="col-sm">
           <label for="file" class="bold-label">Justificatif</label>
             <div class='input-field input-flex file-flex'>
-            <span id="file-name-admin">${bill.name}</span>
+            <span id="file-name-admin">${bill.fileName}</span>
             <div class='icons-container'>
               <span id="icon-eye-d" data-testid="icon-eye-d" data-bill-url="${bill.fileUrl}"> ${eyeWhite} </span>
             </div>
