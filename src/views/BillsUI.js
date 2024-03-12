45590a1edad6936f18975fb33b5951ac6727da75
mmdev73 <mm.dev73@gmail.com>
2024-03-12 14:44:34 +0100

    :bug: correction of issue 2. Bills must be displayed in reverse date order

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,14 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  // correction of issue 2 : bills must be diplayed in reverse order
+  if(data && data.length) {
+    data = data.sort((a, b) => new Date(b.date) - new Date(a.date))
+    return data.map(bill => row(bill)).join("")
+  }
+  return ""
+  // old stuff
+  // return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
